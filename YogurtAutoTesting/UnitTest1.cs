5aa03586cd2ff3c419c98d1a8e79cc1026e88178
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-07-26 10:29:09 +0300

    Initial commit

---
+++ YogurtAutoTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace YogurtAutoTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
