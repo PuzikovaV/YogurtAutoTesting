6c12495cf5dbf782e71148a528890259130bd141
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-07-26 17:17:38 +0300

    Made authorizathion model

---
+++ YogurtAutoTesting/Models/Request/AuthRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YogurtAutoTesting.Models.Request
+{
+    public class AuthRequestModel
+    {
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+    }
+}
