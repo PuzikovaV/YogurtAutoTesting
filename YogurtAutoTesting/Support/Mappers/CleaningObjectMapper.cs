f1814e4e5c1939ab09d2571a11df5cd499dbac17
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-08-05 10:25:05 +0300

    Add delete cleaning object test

---
+++ YogurtAutoTesting/Support/Mappers/CleaningObjectMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using YogurtAutoTesting.Models.Request;
+using YogurtAutoTesting.Models.Response;
+
+namespace YogurtAutoTesting.Support.Mappers
+{
+    public class CleaningObjectMapper
+    {
+        public CleaningObjectResponseModel MappCleaningObjectRequestModelToCleaningObjectResponseModel(CleaningObjectRequestModel model)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<CleaningObjectRequestModel, CleaningObjectResponseModel>());
+            Mapper mapper = new Mapper(config);
+            var responseModel = mapper.Map<CleaningObjectResponseModel>(model);
+            return responseModel;
+        }
+    }
+}
--- YogurtAutoTesting/Tests/TestSources/AddCleaningObject_WhenModelIsCorrect_TestSource.cs
+++ YogurtAutoTesting/Tests/TestSources/AddCleaningObject_WhenModelIsCorrect_TestSource.cs
@@ -7,34 +7,16 @@ namespace YogurtAutoTesting.Tests.TestSources
     {
         public IEnumerator GetEnumerator()
         {
-            yield return new object[]
+            yield return new CleaningObjectRequestModel()
             {
-                new ClientRequestModel()
-                {
-                    FirstName = "Константин",
-                    LastName = "Придуманный",
-                    BirthDate = new DateTime(1966, 06, 16, 00, 00, 00),
-                    Password = "thebestKostya666",
-                    ConfirmPassword = "thebestKostya666",
-                    Email = "kostik@gmail.com",
-                    Phone = "89996662233"
-                },
-                new AuthRequestModel()
-                {
-                    Email = "kostik@gmail.com",
-                    Password = "thebestKostya666",
-                },
-                new CleaningObjectRequestModel()
-                {
-                    NumberOfRooms = 3,
-                    NumberOfBathrooms = 2,
-                    Square = 70,
-                    NumberOfWindows = 6,
-                    NumberOfBalconies = 2,
-                    Address = "ул. Ленина д. 48, кв. 3",
-                    District = 2
-                }
-
+                NumberOfRooms = 3,
+                NumberOfBathrooms = 2,
+                Square = 70,
+                NumberOfWindows = 6,
+                NumberOfBalconies = 2,
+                Address = "ул. Ленина д. 48, кв. 3",
+                District = 2,
+                ClientId = 0
             };
         }
     }
