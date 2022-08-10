a3b1537e03ee5e92cafac933cf4ef569fbf4da9f
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-08-10 12:25:01 +0300

    Add create cleaner test

--- YogurtAutoTesting/Tests/RegisterCleanerTest.cs
+++ YogurtAutoTesting/Tests/RegisterCleanerTest.cs
@@ -1,12 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using YogurtAutoTesting.Models.Request;
+using YogurtAutoTesting.Models.Response;
+using YogurtAutoTesting.Support;
+using YogurtAutoTesting.Tests.StepDefinitions;
 
 namespace YogurtAutoTesting.Tests
 {
     public class RegisterCleanerTest
     {
+        private AuthorizationSteps _authorizationSteps;
+        private CleanerSteps _cleanerSteps;
+        private ServiceSteps _serviceSteps;
+        private BaseClearCommand _deleteFromDb;
+        int _serviceId;
+        string _adminToken;
+
+        public RegisterCleanerTest()
+        {
+            _authorizationSteps = new AuthorizationSteps();
+            _cleanerSteps = new CleanerSteps();
+            _serviceSteps = new ServiceSteps();
+            _deleteFromDb = new BaseClearCommand();
+        }
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _deleteFromDb.ClearBase();
+
+            AuthRequestModel authModel = new AuthRequestModel()
+            {
+                Email = "Admin@gmail.com",
+                Password = "qwerty12345",
+            };
+            _adminToken = _authorizationSteps.Authorize(authModel);
+
+            ServicesRequestModel requestModel = new ServicesRequestModel()
+            {
+                Name = "Помыть микроволновку",
+                Price = 300.00,
+                Unit = "Кухня",
+                Duration = 15
+            };
+            _serviceId = _serviceSteps.CreateServiceTest(requestModel, _adminToken);
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _deleteFromDb.ClearBase();
+        }
+        [Test]
+        public void RegisterCleaner_WhenModelIsCorrect_ShouldCreateCleaner()
+        {
+
+            CleanerRequestModel cleanerModel = new CleanerRequestModel()
+            {
+                FirstName = "Зина",
+                LastName = "Корзина",
+                BirthDate = new DateTime(1990, 08, 09),
+                Email = "korzinka@zinka.ru",
+                Password = "ZiZi1234",
+                ConfirmPassword = "ZiZi1234",
+                Passport = "1254789654",
+                Phone = "89998887744",
+                Schedule = 1,
+                Districts = new List<int>() { 5, 6, 8 },
+                ServicesIds = new List<int>(_serviceId)
+            };
+
+            DateTime regDate = DateTime.Now.Date;
+
+            int cleanerId = _cleanerSteps.CreateCleanerTest(cleanerModel);
+
+            AuthRequestModel cleanerAuthModel = new AuthRequestModel()
+            {
+                Email = cleanerModel.Email,
+                Password = cleanerModel.Password,
+            };
+
+            string token = _authorizationSteps.Authorize(cleanerAuthModel);
+
+            CleanerResponseModel expectedCleaner = new CleanerResponseModel()
+            {
+                Id = cleanerId,
+                FirstName = cleanerModel.FirstName,
+                LastName = cleanerModel.LastName,
+                BirthDate = cleanerModel.BirthDate,
+                Email = cleanerModel.Email,
+                Phone = cleanerModel.Phone,
+                RegistrationDate = regDate
+            };
+
+            _cleanerSteps.GetCleanerById(cleanerId, token, expectedCleaner);
+        }
     }
 }
--- YogurtAutoTesting/Tests/StepDefinitions/CleanerSteps.cs
+++ YogurtAutoTesting/Tests/StepDefinitions/CleanerSteps.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YogurtAutoTesting.HttpClients;
+using YogurtAutoTesting.Models.Request;
+using YogurtAutoTesting.Models.Response;
+using System.Text.Json;
 
 namespace YogurtAutoTesting.Tests.StepDefinitions
 {
     public class CleanerSteps
     {
+        private CleanerClient _cleanerClient;
+
+        public CleanerSteps()
+        {
+            _cleanerClient = new CleanerClient();
+        }
+
+        public int CreateCleanerTest(CleanerRequestModel model)
+        {
+            HttpContent httpContent = _cleanerClient.RegisterCleaner(model, HttpStatusCode.Created);
+            string content = httpContent.ReadAsStringAsync().Result;
+            int cleanerId = Convert.ToInt32(content);
+
+            Assert.IsTrue(cleanerId > 0);
+
+            return cleanerId;
+        }
+
+        public CleanerResponseModel GetCleanerById(int id, string token, CleanerResponseModel expected)
+        {
+            HttpContent httpContent = _cleanerClient.GetCleanerById(id, token, HttpStatusCode.OK);
+            string content = httpContent.ReadAsStringAsync().Result;
+
+            CleanerResponseModel actual = JsonSerializer.Deserialize<CleanerResponseModel>(content);
+
+            Assert.AreEqual(expected, actual);
+
+            return actual;
+        }
+
     }
 }
