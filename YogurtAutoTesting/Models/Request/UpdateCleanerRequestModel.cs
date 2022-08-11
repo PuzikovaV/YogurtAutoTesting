52016371fa814a5856280196b132452ea971a04d
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-08-11 11:07:56 +0300

    Add update cleaner test

--- YogurtAutoTesting/Models/Request/UpdateCleanerRequestModel.cs
+++ YogurtAutoTesting/Models/Request/UpdateCleanerRequestModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace YogurtAutoTesting.Models.Request
 {
-    internal class UpdateCleanerRequestModel
+    public class UpdateCleanerRequestModel
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("birthDate")]
+        public DateTime BirthDate { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("servicesIds")]
+        public List<int> ServicesIds { get; set; }
+
+        [JsonPropertyName("districts")]
+        public List<int> Districts { get; set; }
     }
 }
---
+++ YogurtAutoTesting/Tests/TestSources/AddCleaner_WhenModelIsCorrect_TestCaseSource.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using YogurtAutoTesting.Models.Request;
+
+namespace YogurtAutoTesting.Tests.TestSources
+{
+    public class AddCleaner_WhenModelIsCorrect_TestCaseSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new AuthRequestModel()
+                {
+                    Email = "Admin@gmail.com",
+                    Password = "qwerty12345",
+                },
+                new ServicesRequestModel()
+                {
+                    Name = "Помыть микроволновку",
+                    Price = 300.00,
+                    Unit = "Кухня",
+                    Duration = 15
+                },
+                new CleanerRequestModel()
+                {
+                    FirstName = "Зина",
+                    LastName = "Корзина",
+                    BirthDate = new DateTime(1990, 08, 09),
+                    Email = "korzinka@zinka.ru",
+                    Password = "ZiZi1234",
+                    ConfirmPassword = "ZiZi1234",
+                    Passport = "1254789654",
+                    Phone = "89998887744",
+                    Schedule = 1,
+                    Districts = new List<int>() { 5, 6, 8 }
+                }
+            };
+        }
+    }
+}
